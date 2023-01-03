8f9e0b59de7ca11e1ee619329d204a204e08cdb5
kevkev14 <kevi5598@stud.kea.dk>
2023-01-03 16:05:40 +0100

    lavet JS til portfolio cards

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ tabsContainer.addEventListener("click", (e) => {
     aboutSection.querySelector(target).classList.add("active");
   }
 });
+
+/* -------- PORTFOLIO "ITEM DETAIL" SEKTION -------- */
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("view-project-btn")) {
+    togglePortfolioPopup();
+  }
+});
+function togglePortfolioPopup() {
+  document.querySelector(".portfolio-popup").classList.toggle("open");
+  document.body.classList.toggle("hide-scrolling");
+  document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
