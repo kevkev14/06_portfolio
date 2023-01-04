51e9638348bf740574aa87a5f1d9baa7e157fa08
kevkev14 <kevi5598@stud.kea.dk>
2023-01-04 01:35:14 +0100

    Endeligt website ift funktioner (mangler stadig me

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+/* ---------------------------------------- NAVIGATION BAR / BURGERMENU SEKTION ---------------------------------------- */
+//Gør så at når man klikker på burgerbar knappen at siden fader væk med keyframes i CSS
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+  hideSection();
+  toggleNavbar();
+  document.body.classList.toggle("hide-scrolling");
+});
+//Gør så når at en sektioner bliver gemt væk at den får css reglen "fade out" så den får en seamless effekt
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
+}
+//Gør så navbar bliver togglet med active
+function toggleNavbar() {
+  document.querySelector(".header").classList.toggle("active");
+}
+
+/* ---------------------------------------- ACTIVE FOR ALLE FANER SEKTION ---------------------------------------- */
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+    //Tænder for et overlay så man ikke kan trykke flere gange
+    document.querySelector(".overlay").classList.add("active");
+    //Gør så menu popper frem og man kan vælge en af de 4 sider
+    //Stopper samtidig muligheden for at kunne scroll
+    navToggler.classList.add("hide");
+    if (e.target.classList.contains("nav-item")) {
+      toggleNavbar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+    //Gør at den sektion som bliver trykket på forsvinder ved at fjerne active tagget
+    //Og bagefter giver et nyt active tag til en af de andre sider
+    // Det gør at den tidligere som som var åben bliver lukket og en ny popper frem
+    setTimeout(() => {
+      document.querySelector("section.active").classList.remove("active", "fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      //Overlay blive removet igen efter alt er gået igennem
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
+});
+
 /* ---------------------------------------- ABOUT TABS ---------------------------------------- */
 const tabsContainer = document.querySelector(".about-tabs"),
   aboutSection = document.querySelector(".about-section");
